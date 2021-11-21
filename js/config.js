e07dac1ca6675d8f28d5bf7b76b2ba0a946c4c54
xavien77 <75010389+xavien77@users.noreply.github.com>
2021-11-20 23:23:52 -0600

    config update

--- js/config.js
+++ js/config.js
@@ -1,8 +1,8 @@
 window._config = {
     cognito: {
-        userPoolId: '', // e.g. us-east-2_uXboG5pAb
-        userPoolClientId: '', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
-        region: '' // e.g. us-east-2
+        userPoolId: 'us-east-1_tB8HkjVgI', // e.g. us-east-2_uXboG5pAb
+        userPoolClientId: '488q17oj25mcdp6g6gm8o71o67', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+        region: 'us-east-1' // e.g. us-east-2
     },
     api: {
         invokeUrl: '' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
